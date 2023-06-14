db03b4e227bf9303c8b218683a6733cbce4d3bf9
kaburia <kaburiaaustin1@gmail.com>
2023-06-14 17:32:43 +0300

    documentation

--- filter_stations/setup.py
+++ filter_stations/setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='filter_stations',
-    version='0.3.4',
+    version='0.3.9',
     packages=find_packages(),
     include_package_data=True,
     description='Making it easier to navigate and clean station data',
